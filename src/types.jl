d210ad5eb5eb4df7a107a977836aa20232ddfc7b
Kiruse <contact@kiruse.dev>
2021-05-28 22:50:19 +0200

    Added basic JL Type to GL Enum runtime conversion Types include primitives, vecs & mats

--- src/types.jl
+++ src/types.jl
@@ -2,19 +2,43 @@
 # GraphicsLayer abstract type definitions
 # -----
 # Copyright (c) Kiruse 2021. Licensed under LGPL-2.1
+using StaticArrays
 
-# OpenGL type abstracts
-export GLType, GLPrimitive, GLAbstractPacked, GLSampler
-abstract type GLType end
-abstract type GLPrimitive{B} <: GLType end
-abstract type GLAbstractPacked <: GLType end
-abstract type GLSampler <: GLType end
+export Vec, Vec2, Vec3, Vec4
+const Vec{N, T} = SVector{N, T}
+const Vec2{T} = Vec{2, T}
+const Vec3{T} = Vec{3, T}
+const Vec4{T} = Vec{4, T}
 
-# OpenGL resources
+export Mat, Mat2, Mat3, Mat4
+const Mat{N, M, T} = SMatrix{N, M, T}
+const MatN{N, T} = Mat{N, N, T}
+const Mat2{T} = MatN{2, T}
+const Mat3{T} = MatN{3, T}
+const Mat4{T} = MatN{4, T}
+
 export Buffer, Shader
 abstract type Buffer end
 abstract type Shader end
 
-# Texture abstracts
 export Texture
 abstract type Texture end
+
+
+glname(::Type{Bool})    = :GL_BOOL
+glname(::Type{Int8})    = :GL_BYTE
+glname(::Type{UInt8})   = :GL_UNSIGNED_BYTE
+glname(::Type{Int16})   = :GL_SHORT
+glname(::Type{UInt16})  = :GL_UNSIGNED_SHORT
+glname(::Type{Int32})   = :GL_INT
+glname(::Type{UInt32})  = :GL_UNSIGNED_INT
+glname(::Type{Float32}) = :GL_FLOAT
+glname(::Type{Float64}) = :GL_DOUBLE
+glname(::Type{Vec{N, T}}) where {N, T<:Union{Bool, UInt32, Int32, Float32, Float64}} = N ∈ 2:4 ? Symbol(string(glname(T) * "_VEC$N")) : throw(ArgumentError("invalid vector size $N, expected ∈ 2:4"))
+glname(::Type{Mat{N, N, T}}) where {N, T<:Union{Float32, Float64}} = N ∈ 2:4 ? Symbol(string(glname(T) * "_MAT$N")) : throw(ArgumentError("invalid square matrix size $N, expected ∈ 2:4"))
+glname(::Type{Mat{N, M, T}}) where {N, M, T<:Union{Float32, Float64}} = N ∈ 2:4 && M ∈ 2:4 ? Symbol(string(glname(T) * "_MAT$Nx$M")) : throw(ArgumentError("invalid matrix size $N×$M, expected N,M ∈ 2:4"))
+# TODO: Textures/Samplers, Images
+
+# Generic helper
+glsymbol(name::Symbol) = getproperty(ModernGL, name)
+glsymbol(T::Type) = glsymbol(glname(T))
