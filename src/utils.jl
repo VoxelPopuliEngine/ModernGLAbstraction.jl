5cfd5698b3baae3912f0de6c5d300f8f3c8c6aa0
Kiruse <contact@kiruse.dev>
2021-06-10 23:52:58 +0200

    Added simple abstraction of glGet

--- src/utils.jl
+++ src/utils.jl
@@ -3,6 +3,7 @@
 # -----
 # Copyright (c) Kiruse 2021. Licensed under LGPL-2.1
 using ExtraFun
+using ModernGL
 
 function bytes(x; mapper = identity)
     x = iterable(x)
@@ -12,3 +13,32 @@ function bytes(x; mapper = identity)
     end
     take!(buffer)
 end
+
+function glstate(T::Type, enum::ModernGL.GLenum)
+    ref = Ref{T}()
+    glstate_fun(T)(enum, ref)
+    @glassert begin
+        InvalidEnum => ArgumentError("invalid OpenGL enum")
+    end
+    ref[]
+end
+glstate_fun(::Type{Bool}) = ModernGL.glGetBooleanv
+glstate_fun(::Type{Int32}) = ModernGL.glGetIntegerv
+glstate_fun(::Type{Int64}) = ModernGL.glGetInteger64v
+glstate_fun(::Type{Float64}) = ModernGL.glGetDoublev
+glstate_fun(::Type{Float32}) = ModernGL.glGetFloatv
+
+function glstatei(T::Type, enum::ModernGL.GLenum, idx::Integer)
+    ref = Ref{T}()
+    glstatei_fun(T)(enum, idx, ref)
+    @glassert begin
+        InvalidEnum => ArgumentError("invalid OpenGL enum")
+        InvalidValue => BoundsError(nothing, idx)
+    end
+    val
+end
+glstatei_fun(::Type{Bool}) = ModernGL.glGetBooleani_v
+glstatei_fun(::Type{Int32}) = ModernGL.glGetIntegeri_v
+glstatei_fun(::Type{Int64}) = ModernGL.glGetInteger64i_v
+glstatei_fun(::Type{Float64}) = ModernGL.glGetDoublei_v
+glstatei_fun(::Type{Float32}) = ModernGL.glGetFloati_v
