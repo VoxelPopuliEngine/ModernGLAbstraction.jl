283e9166b0f0cd11f1682622a860c5ce7d64a81c
Kiruse <contact@kiruse.dev>
2021-06-14 01:25:00 +0200

    Added Vec/Mat invariants for convenience

--- src/types.jl
+++ src/types.jl
@@ -6,16 +6,33 @@ using StaticArrays
 
 export Vec, Vec2, Vec3, Vec4
 const Vec{N, T} = SVector{N, T}
-const Vec2{T} = Vec{2, T}
-const Vec3{T} = Vec{3, T}
-const Vec4{T} = Vec{4, T}
+# Generate invariants using `eval`
+for N ∈ 1:4
+    eval(Expr(:const, Expr(:(=), Expr(:curly, Symbol("Vec$N"), :T), Expr(:curly, :Vec, N, :T))))
+    for (suffix, T) ∈ ('b' => Bool, 'i' => Int32, "ui" => UInt32, 'f' => Float32, 'd' => Float64)
+        eval(Expr(:const, Expr(:(=), Symbol("Vec$(N)$(suffix)"), Expr(:curly, :Vec, N, T))))
+    end
+end
 
 export Mat, Mat2, Mat3, Mat4
 const Mat{N, M, T} = SMatrix{N, M, T}
 const MatN{N, T} = Mat{N, N, T}
-const Mat2{T} = MatN{2, T}
-const Mat3{T} = MatN{3, T}
-const Mat4{T} = MatN{4, T}
+for N ∈ 2:4
+    for M ∈ 2:4
+        if N == M
+            eval(Expr(:const, Expr(:(=), Expr(:curly, Symbol("Mat$N"), :T), Expr(:curly, :Mat, N, N, :T))))
+        else
+            eval(Expr(:const, Expr(:(=), Expr(:curly, Symbol("Mat$(N)x$(M)"), :T), Expr(:curly, :Mat, N, M, :T))))
+        end
+        for (suffix, T) ∈ ('b' => Bool, 'i' => Int32, "ui" => UInt32, 'f' => Float32, 'd' => Float64)
+            if N == M
+                eval(Expr(:const, Expr(:(=), Symbol("Mat$(N)$(suffix)"), Expr(:curly, :Mat, N, N, T))))
+            else
+                eval(Expr(:const, Expr(:(=), Symbol("Mat$(N)x$(M)$(suffix)"), Expr(:curly, :Mat, N, M, T))))
+            end
+        end
+    end
+end
 
 export Buffer, Shader
 abstract type Buffer end
@@ -34,9 +51,9 @@ glname(::Type{Int32})   = :GL_INT
 glname(::Type{UInt32})  = :GL_UNSIGNED_INT
 glname(::Type{Float32}) = :GL_FLOAT
 glname(::Type{Float64}) = :GL_DOUBLE
-glname(::Type{Vec{N, T}}) where {N, T<:Union{Bool, UInt32, Int32, Float32, Float64}} = N ∈ 2:4 ? Symbol(string(glname(T) * "_VEC$N")) : throw(ArgumentError("invalid vector size $N, expected ∈ 2:4"))
-glname(::Type{Mat{N, N, T}}) where {N, T<:Union{Float32, Float64}} = N ∈ 2:4 ? Symbol(string(glname(T) * "_MAT$N")) : throw(ArgumentError("invalid square matrix size $N, expected ∈ 2:4"))
-glname(::Type{Mat{N, M, T}}) where {N, M, T<:Union{Float32, Float64}} = N ∈ 2:4 && M ∈ 2:4 ? Symbol(string(glname(T) * "_MAT$Nx$M")) : throw(ArgumentError("invalid matrix size $N×$M, expected N,M ∈ 2:4"))
+glname(::Type{Vec{N, T}}) where {N, T<:Union{Bool, UInt32, Int32, Float32, Float64}} = N ∈ 2:4 ? Symbol(string(glname(T)) * "_VEC$N") : throw(ArgumentError("invalid vector size $N, expected ∈ 2:4"))
+glname(::Type{<:Mat{N, N, T}}) where {N, T<:Union{Float32, Float64}} = N ∈ 2:4 ? Symbol(string(glname(T)) * "_MAT$N") : throw(ArgumentError("invalid square matrix size $N, expected ∈ 2:4"))
+glname(::Type{<:Mat{N, M, T}}) where {N, M, T<:Union{Float32, Float64}} = N ∈ 2:4 && M ∈ 2:4 ? Symbol(string(glname(T)) * "_MAT$Nx$M") : throw(ArgumentError("invalid matrix size $N×$M, expected N,M ∈ 2:4"))
 # TODO: Textures/Samplers, Images
 
 # Generic helper
